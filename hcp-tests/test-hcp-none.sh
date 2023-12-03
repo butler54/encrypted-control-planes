c4304b0ccc7e3fdfca29da697836558a4191a0fa
Chris Butler <chris.butler@redhat.com>
2023-12-04 09:58:02 +1100

    feat: Add Ansible automation platform subscription and controller (#22)
    * feat: Add Ansible automation platform subscription and controller
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: CICD errors
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: Always define values in charts by default
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: Correct location of template values file
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: Markdown linting
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: typos
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: Correct flags missing with current version
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    * fix: Correct markdown again
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>
    
    ---------
    
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

---
+++ hcp-tests/test-hcp-none.sh
@@ -0,0 +1,18 @@
+export CLUSTER_NAME=kubevirt2
+export PULL_SECRET="${HOME}/.config/containers/auth.json"
+export MEM="6Gi"
+export CPU="2"
+export WORKER_COUNT="1"
+export OCP_BASE_DOMAIN=sandbox949.opentlc.com
+
+hypershift create cluster none \
+--name $CLUSTER_NAME \
+--node-pool-replicas $WORKER_COUNT \
+--pull-secret $PULL_SECRET \
+--etcd-storage-class gp3-csi \
+--release-image quay.io/openshift-release-dev/ocp-release:4.13.4-x86_64 \
+--control-plane-availability-policy HighlyAvailable \
+--base-domain ${OCP_BASE_DOMAIN} \
+--generate-ssh \
+--namespace local-cluster \
+--render
