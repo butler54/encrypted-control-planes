e35536d2a81a58b48f91fbbd02efc3abb78d9e8a
Chris Butler <chris.butler@redhat.com>
2024-01-19 14:15:43 +1100

    fix: Correcting name of cluster so it can access the storage class
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

--- hcp-tests/test-hcp-none.sh
+++ hcp-tests/test-hcp-none.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
-export NAME=$1
+export CLUSTER_NAME=$1
 export PULL_SECRET="${HOME}/pull-secret"
 export MEM="6Gi"
 export CPU="2"
 export WORKER_COUNT="1"
-export OCP_BASE_DOMAIN=sandbox949.opentlc.com
+export OCP_BASE_DOMAIN=2g96t.sandbox3015.opentlc.com
 
 hypershift create cluster none \
 --name $CLUSTER_NAME \
@@ -12,7 +12,6 @@ hypershift create cluster none \
 --pull-secret $PULL_SECRET \
 --etcd-storage-class gp3-csi \
 --release-image quay.io/openshift-release-dev/ocp-release:4.13.4-x86_64 \
---control-plane-availability-policy HighlyAvailable \
+--control-plane-availability-policy SingleReplica \
 --base-domain ${OCP_BASE_DOMAIN} \
---generate-ssh \
---namespace local-cluster
+--generate-ssh
